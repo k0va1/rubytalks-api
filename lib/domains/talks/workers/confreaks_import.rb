5474971026bd4f8f7bf2039b9eea01ee824e8ca9
k0va1 <al3xander.koval@gmail.com>
2020-02-15 23:21:08 +0300

    add worker for confreaks import

---
+++ lib/domains/talks/workers/confreaks_import.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+module Domains
+  module Talks
+    module Workers
+      class ConfreaksImport
+        include Dry::Monads::Result::Mixin
+        include Sidekiq::Worker
+
+        include Import[
+          :logger,
+          operation: 'domains.talks.operations.importer.confreaks'
+        ]
+
+        def perform
+          result = operation.call
+
+          case result
+          when Success
+            # TODO: log some info about how many talks, events and speakers have been imported
+            logger.info("#{result.value![:amount]} talks successfully imported")
+          when Failure
+            logger.error('Could not import talks from confreaks.')
+          end
+        end
+      end
+    end
+  end
+end
