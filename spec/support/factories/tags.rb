2a56cbc864475189b676376a00a0c66ee4550bd4
k0va1 <al3xander.koval@gmail.com>
2020-04-07 21:54:45 +0300

    Add tag factory

---
+++ spec/support/factories/tags.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Factory.define(:tag) do |f|
+  f.sequence(:title) { |n| "tag#{n}"}
+  f.sequence(:slug) { |n| "tag#{n}"}
+  f.state { 'unpublished' }
+  f.updated_at { Time.now }
+  f.created_at { Time.now }
+end
