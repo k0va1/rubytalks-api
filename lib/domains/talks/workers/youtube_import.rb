abfd295eedc2bebbde52b795d241434c502aeb51
k0va1 <al3xander.koval@gmail.com>
2020-05-01 21:42:46 +0300

    Add worker for youtube import

---
+++ lib/domains/talks/workers/youtube_import.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Domains
+  module Talks
+    module Workers
+      class YoutubeImport
+        include Dry::Monads::Result::Mixin
+        include Sidekiq::Worker
+
+        include Import[
+          operation: 'domains.talks.operations.importer.youtube',
+          logger: 'app_logger'
+        ]
+
+        def perform
+          result = operation.call
+
+          case result
+          when Success
+            # TODO: log some info about how many talks, events and speakers have been imported
+            # kind of ImportStatistics or smth.
+            logger.info('Talks have successfully imported')
+          when Failure
+            logger.error('Could not import talks from youtube.')
+          end
+        end
+      end
+    end
+  end
+end
