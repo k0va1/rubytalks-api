a4a06d358d0bd441b5eb39cf420a377e45001866
k0va1 <al3xander.koval@gmail.com>
2020-05-01 21:26:57 +0300

    Rename `my_logger` to `app_logger`

--- system/boot/logger.rb
+++ system/boot/logger.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-Hanami::Container.boot(:my_logger) do |app|
+Hanami::Container.boot(:app_logger) do |app|
   init do
     use :dotenv
 
     SemanticLogger.default_level = ENV.fetch('LOGGER_LEVEL')
     SemanticLogger.add_appender(io: $stdout, formatter: :color)
-    app.register(:my_logger, SemanticLogger['RubyTalks'])
+    app.register(:app_logger, SemanticLogger['RubyTalks'])
   end
 end
