660f343b1f8902796e6412eb5d5c02cb66903288
k0va1 <al3xander.koval@gmail.com>
2020-05-06 14:24:01 +0300

    Add approved action to Speakers

---
+++ apps/admin_api/actions/speakers/approved.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module AdminApi
+  module Actions
+    module Speakers
+      class Approved < AdminApi::Action
+        include Dry::Monads::Result::Mixin
+        include AppImport[
+          speakers: 'admin_api.services.speakers'
+        ]
+
+        params do
+          optional(:page).filled(:integer)
+          optional(:per_page).filled(:integer)
+        end
+
+        def handle(request, response)
+          input = validate_params(request.params)
+          result = speakers.approved_speakers_list(input)
+
+          respond_with_collection(response, result, Serializers::Speaker)
+        end
+      end
+    end
+  end
+end
